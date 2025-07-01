59e7144d05fd75cecc0ac385fcd917245abba379
vijaypawar29 <vijaypawar2998@gmail.com>
2025-07-01 16:09:07 +0530

    change made in index script styles

---
+++ script.js
@@ -0,0 +1,32 @@
+// This is a basic JavaScript example
+
+// Variables
+let message = "Hello, World!";
+console.log(message);
+
+// Functions
+function addNumbers(a, b) {
+    return a + b;
+}
+console.log(addNumbers(5, 3));
+
+// Conditional Statements
+let number = 10;
+if (number > 5) {
+    console.log("Number is greater than 5");
+} else {
+    console.log("Number is 5 or less");
+}
+
+// Loops
+for (let i = 0; i < 5; i++) {
+    console.log("Iteration:", i);
+}
+
+// Arrays
+let fruits = ["Apple", "Banana", "Cherry"];
+fruits.forEach(fruit => console.log(fruit));
+
+
+
+
